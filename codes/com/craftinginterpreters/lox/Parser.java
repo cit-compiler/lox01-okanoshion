ebaf869a7ba3a8080daa3d7eeb761f8588bd3075
岡野紫音 <cish22083@g.nihon-u.ac.jp>
2024-12-24 10:23:55 +0900

    add GenerateAst.java, generate Expr.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -87,6 +87,7 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+    throw error(peek(), "Expect expression.");
   }
 
   private boolean match(TokenType... types) {
@@ -131,5 +132,35 @@ class Parser {
     return new ParseError();
   }
 
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
+
 
 }
+
+Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
