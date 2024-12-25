c47ebfb22e91af948dcc3034c15ac61caf8e82f4
岡野紫音 <cish22083@g.nihon-u.ac.jp>
2024-12-26 00:01:32 +0900

    add Interpreter.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -27,6 +29,7 @@ public class Lox {
     run(new String(bytes, Charset.defaultCharset()));
 
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -50,7 +53,7 @@ public class Lox {
 
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
@@ -70,6 +73,12 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
 }
