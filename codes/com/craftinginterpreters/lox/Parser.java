68a43d15e6aa34be3dc48a7c43c922fd2d4a3ca2
岡野紫音 <cish22083@g.nihon-u.ac.jp>
2024-12-25 23:26:49 +0900

    modify Parser.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {}
   private final List<Token> tokens;
   private int current = 0;
 
@@ -102,6 +103,12 @@ class Parser {
     return false;
   }
 
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
   private boolean check(TokenType type) {
     if (isAtEnd()) return false;
     return peek().type == type;
@@ -124,5 +131,32 @@ class Parser {
     return tokens.get(current - 1);
   }
 
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
 
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
+
+  
 }
