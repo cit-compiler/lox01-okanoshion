1b307402ce57aff559e952f8e701064788688257
岡野紫音 <cish22083@g.nihon-u.ac.jp>
2025-01-16 22:05:01 +0900

    add Environment.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -17,7 +17,7 @@ class Parser {
    List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
     return statements; 
@@ -37,6 +37,17 @@ class Parser {
     return equality();
   }
 
+  private Stmt declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
   private Stmt statement() {
     if (match(PRINT)) return printStatement();
 
@@ -48,6 +59,19 @@ class Parser {
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
   }
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
+
 
   private Stmt expressionStatement() {
     Expr expr = expression();
@@ -121,6 +145,10 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
